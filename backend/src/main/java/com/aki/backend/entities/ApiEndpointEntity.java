f58702fdc690f6c6a60f1d294ee9b974c0ab9b42
st187509 <st187509@stud.uni-stuttgart.de>
2025-11-03 16:31:18 +0100

    Initial backend

---
+++ backend/src/main/java/com/aki/backend/entities/ApiEndpointEntity.java
@@ -0,0 +1,10 @@
+package com.aki.backend.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "api_endpoints")
+public class ApiEndpointEntity {
+    
+}
